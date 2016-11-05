caccb436c4b99326701256adc6873057fbe53b7c
Iva <iva.gradecak@gmail.com>
2016-11-05 20:18:05 +0000

    User Models with points and levels

--- code_for_good/base/models.py
+++ code_for_good/base/models.py
@@ -2,4 +2,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User)
+
+    points = models.IntegerField(default = 0)
+    level = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.user.username
+
